a27ab160bcc529e38095cd2fa7fa4b8073b0c92b
Raymond Templo <rtemplo@gmail.com>
2017-10-27 03:53:24 -0400

    all done!

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,15 @@
+// utility class to toggle the note saving controls
+function toggleModalControls(savenew) {
+    if (savenew) {
+        $("#save-controls").show();
+        $("#update-controls").hide();
+    } else {
+        $("#save-controls").hide();
+        $("#update-controls").show();
+    }
+}
+
+// go to html routes
 $(document).ready(function () {
     
     $("#home-link").on("click", function () {
@@ -32,13 +44,25 @@ $(document).on("click", ".save-article", function() {
     });
 });
 
+$("#new-note").on("click", function () {
+    //clear the hidden input field
+    $("#noteID").val("");
+
+    //clear the inputs
+    $("#note-title").val("");
+    $("#note-body").val("");
+
+    //show the save controls
+    toggleModalControls(true);
+});
+
+// Delete an Article and all associated Notes
 $(document).on("click", ".delete-article", function() {
     var articleObj = $(this).closest(".article");
 
     $.ajax({
         url: "/articles/" + $(this).attr("data-id"),
-        type: "DELETE",
-        data: articleObj
+        type: "DELETE"
     }).then(function (result) {
         //show a modal here
         if (result) {
@@ -47,80 +71,131 @@ $(document).on("click", ".delete-article", function() {
     });    
 }); 
 
+// Action to launch modal that shows existing notes + form to add a new note
 $(document).on("click", ".view-note", function () {
+    var title = $(this).attr("data-title");
+    var articleID = $(this).attr("data-id");
+
+    $("#note-layer .saved-note").not(":first").remove();
+
+    // set the modal header to the title of the article
+    $("#notes-modal").find(".modal-title").text(title);
+
+    // put articleID in a hidden input
+    $("#articleID").val(articleID);
+    // clear the noteID in case it was populated by a previous note update
+    $("#noteID").val("");
+    
+    //default to save mode
+    toggleModalControls(true);
+
+    // clear the input and textbox in the modal
+    $("#note-title").val("");
+    $("#note-body").val("");
+
+    // get all existing notes, construct html, and append them to note-layer
+    $.ajax({
+        url: "/articles/" + articleID,
+        method: "GET"
+    }).then(function (result) {
+        if (result.notes.length > 0) {
+            for (var i=0; i < result.notes.length; i++) {
+                writeToNoteList(result.notes[i]._id, result.notes[i].title, result.notes[i].body)
+            }
+        }
+    });
+
     $("#notes-modal").modal('show');
 });
 
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-        // Display the apropos information on the page
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
+function writeToNoteList(id, title, body) {
+    // make a new note from the templated note, remove the id, remove the class that is hiding it
+    var newNote = $("#note-template").clone().prop('id', '').removeClass('dontshow');
+    //set the id to the id of the note
+    newNote.attr("id", id);
+    //set the title of the note
+    newNote.find("#note-text-area").html(title);
+    //embed the note body to a data attribute
+    newNote.find("#note-text-area").attr("data-body", body);
+    //append the new note to the notes layer
+    $("#note-layer").append(newNote);    
+}
+
+// When a saved note is clicked on
+$(document).on("click", ".saved-note", function () {
+    // move the contents of the saved note to the the input and textarea
+    $("#note-title").val($(this).find("#note-text-area").text());
+    $("#note-body").val($(this).find("#note-text-area").attr("data-body"));
+
+    // set the noteID in the hidden input
+    $("#noteID").val($(this).attr("id"));
+
+    // hide the save controls for a new note and show the update existing note controls
+    toggleModalControls(false);
 });
-  
-  
-  // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
-    // Empty the notes from the note section
-    $("#notes").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
-  
-    // Now make an ajax call for the Article
+
+// Action to save a note
+$(document).on("click", "#save-note, #update-note", function () {
+    //input validation
+    if ($("#note-title").val().trim() == "" || $("#note-body").val().trim() == "") {
+        alert("Please enter a title and body for the note");
+        return false;
+    }
+
+    var formData = $("#note-form").serializeArray();
+
+    var paramsData = {};
+    formData.forEach(dataunit => {
+        paramsData[dataunit.name] = dataunit.value
+    });
+
+    var url = "/articles/" + paramsData.articleID;
+
     $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .done(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
+        url: url,
+        method: "POST",
+        data: formData
+    }).then(function (result) {
+        if (result) {
+            var id = result.notes[result.notes.length -1]._id;            
+            var title = result.notes[result.notes.length -1].title;
+            var body = result.notes[result.notes.length -1].body;
+
+            writeToNoteList(id, title, body);
+
+            //clear the form inputs
+            $("#note-title").val("");
+            $("#note-body").val("")
+        } else {
+            alert("Sorry something went wrong. The note could not be saved.");
         }
-      });
-  });
-  
-  // When you click the savenote button
-  $(document).on("click", "#savenote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
+    });
+});
+
+// Action to Delete a Note
+$(document).on("click", ".note-delete", function () {
+    var noteObj = $(this).closest(".saved-note");
+
     $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
-      }
-    })
-      // With that done
-      .done(function(data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
-  
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
-  
+        url: "/notes/" + noteObj.attr("id"),
+        type: "DELETE",
+        data: {articleID:$("#articleID").val()}
+    }).then(function (result) {
+        //show a modal here
+        if (result) {
+            //remove the note UI object on the modal window.
+            noteObj.remove();
+
+            //  Make sure there is no noteID because save and update share the same route and this 
+            //  will make it think its an update if it accidentally gets passed back on a subsequent save
+            $("#noteID").val("");
+            
+            //default to save mode
+            toggleModalControls(true);
+        
+            // clear the input and textbox in the modal
+            $("#note-title").val("");
+            $("#note-body").val("");            
+        }
+    });  
+});
--- routes/routes.js
+++ routes/routes.js
@@ -35,7 +35,7 @@ module.exports = function(app, axios, cheerio) {
             Leaving the commented out code below for future reference on how to bulk load data to Mongo via Mongoose
             */
             // Create a new the all the articles all at once with the articlesToSaveObj array
-            // db.Article.create(articlesToSaveObj).then(function(dbArticle) {
+            // db.Article.create(scrapedArticlesArr).then(function(dbArticle) {
             //     // Send the number of articles that were saved
             //     res.json({saved:dbArticle.length });
             // }).catch(function(err) {
@@ -56,10 +56,11 @@ module.exports = function(app, axios, cheerio) {
         });
     });
 
+    // Save an Article but prevent duplicates with upsert (and also maintains the Notes ref by manually setting notes to an empty array)
     app.post("/articles", function (req, res) {
         db.Article.findOneAndUpdate(
             { title:req.body.title }, 
-            { $set:{ link:req.body.link } }, 
+            { $set:{ link:req.body.link, notes:[] } }, 
             { upsert:true, new:true },
             function (err, result) {
                 res.json(result);
@@ -67,30 +68,108 @@ module.exports = function(app, axios, cheerio) {
         );
     });
 
+    // Delete an Article and all associated Notes
     app.delete("/articles/:id", function(req, res) {    
-        db.Article.remove({_id:ObjectId(req.params.id)}).then(function(result) {
-            res.json(result);
+        var id = req.params.id
+
+        db.Article.findOne({ _id: id }).populate("notes").then(function (result) {
+            //gather up the ids of the notes associated to an article
+            var notesIDArr = [];
+            for (var i=0; i < result.notes.length; i++) {
+                notesIDArr.push(ObjectId(result.notes[i].id));
+            }
+
+            //remove all notes associated with the article
+            db.Note.remove({_id:{$in:notesIDArr}}).then(function (deleted) {
+                db.Article.remove({_id:ObjectId(id)}).then(function(result) {
+                    res.json(result);
+                });            
+            });
         });
+
     });
 
     // Retrieve specific Article by id and populate it's note field, then send back the object representing the article to the client side
     app.get("/articles/:id", function(req, res) {
         // find the target article, populate it's note reference field, then return it to the client side
-        db.Article.findOne({ _id: req.params.id }).populate("note").then(function(dbArticle) {
+        db.Article.findOne({ _id: req.params.id }).populate("notes").then(function(dbArticle) {
             res.json(dbArticle);
         }).catch(function(err) {
             res.json(err);
         });
     });
   
-    // Route for saving a Note and associating it to an article
+    // Route for saving/updating a Note and associating it to an article
     app.post("/articles/:id", function(req, res) {
-        db.Note.create(req.body).then(function(dbNote) {
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push:{ note: dbNote._id } }, { new: true });
-        }).then(function(dbArticle) {
-            res.json(dbArticle);
-        }).catch(function(err) {
-            res.json(err);
+        var queryFilter;
+
+        if (req.body.noteID == "") {
+            // match update by title - could be a new note, if a note with the same title already exists then it's body is updated.
+            queryFilter = { title:req.body['note-title'] };
+        } else {
+            // match explicitly by id - this is an update an in this case the title itself can be changed.
+            queryFilter = {_id:req.body.noteID};
+        }
+        
+        db.Note.findOneAndUpdate(
+            queryFilter, 
+            { $set:{title: req.body['note-title'], body:req.body['note-body']} }, 
+            { upsert:true, new:true }
+        ).then(function(dbNote) {
+
+            //get the associated note ids
+            db.Article.findOne({_id:req.params.id}).then(function (data) {
+
+                //check to see if the note is already associated, if it is don't push the id to the Articles reference array
+                var insertCheck = true;
+                for (var i=0; i < data.notes.length; i++) {
+                    if (ObjectId(data.notes[0].id).toString() == dbNote._id.toString()) {
+                        insertCheck = false;
+                    }
+                }
+                
+                if (insertCheck) {
+                    db.Article.findOneAndUpdate(
+                        { _id: req.params.id }, 
+                        { $push:{ notes: dbNote._id } }, 
+                        { upsert:true, new:true }
+                    ).populate("notes").then(function (dbArticle) {
+                        res.json(dbArticle);
+                    }).catch(function(err) {
+                        res.json(err);
+                    });;
+                }
+            });
+        })
+    });
+
+    // Delete an Note and it's ref in Articles
+    app.delete("/notes/:id", function(req, res) {
+        var noteID = ObjectId(req.params.id);
+
+        db.Note.remove({_id:noteID}).then(function (result) {
+            db.Article.findOne({_id:ObjectId(req.body.articleID)}).then(function (result) {
+                var noteRefs = [];
+
+                //get all the note references and put them on an array that we will mutate
+                for (i=0; i < result.notes.length; i++) {
+                    noteRefs.push(result.notes[0].toString())
+                }
+
+                var x = noteRefs.length
+                while (x--) {
+                    if (noteRefs[x] == req.params.id) noteRefs.splice(x, 1);
+                }                
+
+                //put it back to type ObjectId
+                for (i=0; i < noteRefs.length; i++) {
+                    noteRefs[i] = ObjectId(noteRefs[i]) ;
+                }
+                
+                db.Article.findOneAndUpdate({_id:ObjectId(req.body.articleID)}, { $set:{notes:noteRefs} })
+            });
+
+            res.json(result);
         });
     });
   
